3ed663232f1f8d06fe8b3836e5aa6b22321bb41f
Lucas Simpson <lucassimpson0213@gmail.com>
2026-02-05 15:55:20 -0500

    changed surround

--- lua/theprimeagen/lazy/surround.lua
+++ lua/theprimeagen/lazy/surround.lua
@@ -1,8 +1,20 @@
 return {
     "kylechui/nvim-surround",
-    version = "*", -- use for stability
+    version = "*",
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup()
-    end
+        require("nvim-surround").setup({
+            keymaps = {
+                normal = "ys",
+                normal_cur = "yss",
+                normal_line = "yS",
+                normal_cur_line = "ySS",
+                visual = "S",
+                visual_line = "gS",
+                delete = "ds",
+                change = "cs",
+                change_line = "cS",
+            },
+        })
+    end,
 }
