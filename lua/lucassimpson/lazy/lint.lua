09891b422aa587838daebce54df3683df16c7b3e
Lucas Simpson <lucas.simpson0213@gmail.com>
2026-07-22 18:31:00 -0500

    added my name instead of primes

--- lua/lucassimpson/lazy/lint.lua
+++ lua/lucassimpson/lazy/lint.lua
@@ -21,7 +21,6 @@ return {
         lint.linters_by_ft = {
             sh = { "shellcheck" },
             python = { "ruff" },
-            lua = { "luacheck" },
             typescript = { "eslint_d" },
             json = { "jsonlint" },
             yaml = { "cfn_lint" },
