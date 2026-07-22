a9db1330607a947aff1e9cdbf422247d4fbfae5e
Lucas Simpson <lucas.simpson0213@gmail.com>
2026-07-22 18:02:31 -0500

    added my name instead of prime's

---
+++ lua/lucassimpson/lazy/lint.lua
@@ -0,0 +1,38 @@
+return {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufWritePost" },
+    config = function()
+        local lint = require("lint")
+
+
+
+        lint.linters.cfn_lint = {
+            cmd = "cfn-lint",
+            stdin = false,
+            args = { "--format", "parseable" },
+            stream = "stdout",
+            parser = require("lint.parser").from_errorformat(
+                "%f:%l:%c: %t%*[0-9] %m",
+                {
+                    source = "cfn-lint",
+                }
+            )
+        }
+        lint.linters_by_ft = {
+            sh = { "shellcheck" },
+            python = { "ruff" },
+            lua = { "luacheck" },
+            typescript = { "eslint_d" },
+            json = { "jsonlint" },
+            yaml = { "cfn_lint" },
+            yml = { "cfn_lint" }
+
+        }
+
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+    end,
+}
---
+++ lua/lucassimpson/lazy/lsp.lua
@@ -0,0 +1,183 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "stevearc/conform.nvim",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+    },
+
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                javascript = { 'prettier' },
+                python = { 'black' },
+                lua = { 'stylua' },
+                markdown = { 'prettier' },
+                sh = { 'shfmt' },
+
+            }
+        })
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "bashls",
+                "basedpyright",
+                "lua_ls",
+                "rust_analyzer",
+                "vtsls",
+                "tailwindcss",
+
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+
+                zls = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.zls.setup({
+                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                        settings = {
+                            zls = {
+                                enable_inlay_hints = true,
+                                enable_snippets = true,
+                                warn_style = true,
+                            },
+                        },
+                    })
+                    vim.g.zig_fmt_parse_errors = 0
+                    vim.g.zig_fmt_autosave = 0
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                format = {
+                                    enable = true,
+                                    -- Put format options here
+                                    -- NOTE: the value should be STRING!!
+                                    defaultConfig = {
+                                        indent_style = "space",
+                                        indent_size = "2",
+                                    }
+                                },
+                            }
+                        }
+                    }
+                end,
+                ["clangd"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.clangd.setup({
+                        cmd = { "clangd", "--background-index", "--clang-tidy" },
+                    })
+                end,
+                ["rust_analyzer"] = function()
+                    local lspconfig = require("lspconfig")
+                    require("lspconfig").rust_analyzer.setup({
+                        settings = {
+                            ["rust-analyzer"] = {
+                                cargo = {
+                                    target = "i686-os.json",
+                                    buildScripts = { enable = false },
+                                },
+                            },
+                        },
+                    })
+                end,
+                ["asm_lsp"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.asm_lsp.setup({})
+                end,
+                ["tailwindcss"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.tailwindcss.setup({
+                        capabilities = capabilities,
+                        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte", "heex" },
+                    })
+                end,
+            }
+        })
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = "copilot", group_index = 2 },
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            signs = true,
+
+            -- keep the red underline
+            underline = true,
+
+            -- this is the IMPORTANT part (short inline message)
+            virtual_text = {
+                current_line = true, -- only show for the line your cursor is on
+                spacing = 2,
+                prefix = "●", -- small symbol before message
+
+                -- shorten Rust's long essays into useful hints
+                format = function(diagnostic)
+                    local msg = diagnostic.message
+                    msg = msg:gsub("\n", " ")  -- remove newlines
+                    msg = msg:gsub("%s+", " ") -- collapse whitespace
+                    return msg
+                end,
+            },
+
+            -- DO NOT insert whole extra lines under code
+            virtual_lines = false,
+
+            -- don't update while typing (prevents flicker)
+            update_in_insert = false,
+
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end
+}
